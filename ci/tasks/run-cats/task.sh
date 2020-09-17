955173c15c8839f4762602b215d4929344bef896
James Pollard <jpollard@pivotal.io>
2020-09-17 17:56:02 -0600

    CI-FIX: pass args through jq correctly

--- ci/tasks/run-cats/task.sh
+++ ci/tasks/run-cats/task.sh
@@ -13,29 +13,46 @@ echo '{}' | jq \
 --arg cf_api_url "api.${DNS_DOMAIN}" \
 --arg cf_apps_url "apps.${DNS_DOMAIN}" \
 --arg cf_admin_password "${CF_ADMIN_PASSWORD}" \
+--arg cf_push_timeout "${CF_PUSH_TIMEOUT}" \
+--arg default_timeout "${DEFAULT_TIMEOUT}" \
+--arg skip_ssl_validation "${SKIP_SSL_VALIDATION}" \
+--arg include_apps "${INCLUDE_APPS}" \
+--arg include_backend_compatability "${INCLUDE_BACKEND_COMPATABILITY}" \
+--arg include_deployments "${INCLUDE_DEPLOYMENTS}" \
+--arg include_detect "${INCLUDE_DETECT}" \
+--arg include_docker "${INCLUDE_DOCKER}" \
+--arg include_internet_dependent "${INCLUDE_INTERNET_DEPENDENT}" \
+--arg include_docker_registry "${INCLUDE_DOCKER_REGISTRY}" \
+--arg include_route_services "${INCLUDE_ROUTE_SERVICES}" \
+--arg include_routing "${INCLUDE_ROUTING}" \
+--arg include_service_discovery "${INCLUDE_SERVICE_DISCOVERY}" \
+--arg include_service_instance_sharing "${INCLUDE_SERVICE_INSTANCE_SHARING}" \
+--arg include_services "${INCLUDE_SERVICES}" \
+--arg include_tasks "${INCLUDE_TASKS}" \
+--arg include_v3 "${INCLUDE_V3}" \
 '{
   "api": $cf_api_url,
   "admin_user": "admin",
   "admin_password": $cf_admin_password,
   "apps_domain": $cf_apps_url,
-  "cf_push_timeout": 150,
-  "default_timeout": 180,
-  "skip_ssl_validation": true,
+  "cf_push_timeout": $cf_push_timeout,
+  "default_timeout": $default_timeout,
+  "skip_ssl_validation": $skip_ssl_validation,
   "timeout_scale": 1,
-  "include_apps": ${INCLUDE_APPS},
-  "include_backend_compatibility": ${INCLUDE_BACKEND_COMPATABILITY},
-  "include_deployments": ${INCLUDE_DEPLOYMENTS},
-  "include_detect": ${INCLUDE_DETECT},
-  "include_docker": ${INCLUDE_DOCKER},
-  "include_internet_dependent": ${INCLUDE_INTERNET_DEPENDENT},
-  "include_private_docker_registry": ${INCLUDE_DOCKER_REGISTRY},
-  "include_route_services": ${INCLUDE_ROUTE_SERVICES},
-  "include_routing": ${INCLUDE_ROUTING},
-  "include_service_discovery": ${INCLUDE_SERVICE_DISCOVERY},
-  "include_service_instance_sharing": ${INCLUDE_SERVICE_INSTANCE_SHARING},
-  "include_services": ${INCLUDE_SERVICES},
-  "include_tasks": ${INCLUDE_TASKS},
-  "include_v3": ${INCLUDE_V3},
+  "include_apps": $include_apps,
+  "include_backend_compatibility": $include_backend_compatability,
+  "include_deployments": $include_deployments,
+  "include_detect": $include_detect,
+  "include_docker": $include_docker,
+  "include_internet_dependent": $include_internet_dependent,
+  "include_private_docker_registry": $include_docker_registry,
+  "include_route_services": $include_route_services,
+  "include_routing": $include_routing,
+  "include_service_discovery": $include_service_discovery,
+  "include_service_instance_sharing": $include_service_instance_sharing,
+  "include_services": $include_services,
+  "include_tasks": $include_tasks,
+  "include_v3": $include_v3,
   "infrastructure": "kubernetes",
   "ruby_buildpack_name": "paketo-community/ruby",
   "python_buildpack_name": "paketo-community/python",
