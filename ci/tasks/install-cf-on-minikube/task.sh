0ee6da6925590eb9cef08a72c478266c57dc2ae1
Andrew Costa <acosta@pivotal.io>
2020-09-30 14:49:49 -0700

    WIP - run on minikube in ci
    [#174483205](https://www.pivotaltracker.com/story/show/174483205)
    
    Co-authored-by: Eric Promislow <epromislow@suse.com>

---
+++ ci/tasks/install-cf-on-minikube/task.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -eou pipefail
+
+source cf-for-k8s-ci/ci/helpers/auth-to-gcp.sh
+
+echo "Generating install values..."
+cf-for-k8s/hack/generate-values.sh -d vcap.me -g gcp-service-account.json > cf-install-values/cf-install-values.yml
+cat <<EOT >> cf-install-values/cf-install-values.yml
+add_metrics_server_components: true
+enable_automount_service_account_token: true
+metrics_server_prefer_internal_kubelet_address: true
+remove_resource_requirements: true
+use_first_party_jwt_tokens: true
+
+load_balancer:
+  enable: false
+EOT
+
+echo "Uploading cf-for-k8s repo..."
+gcloud beta compute \
+  scp --recurse cf-for-k8s ${user_host}:/tmp/minikube/ --compress \
+  --zone "us-central1-a" > /dev/null
+
+echo "Replacing CI directory..."
+gcloud beta compute \
+  scp --recurse cf-for-k8s-ci/ci ${user_host}:/tmp/minikube/cf-for-k8s/ --compress \
+  --zone "us-central1-a" > /dev/null
+
+echo "Uploading cf-install-values.yml..."
+gcloud beta compute \
+  scp cf-install-values/cf-install-values.yml ${user_host}:/tmp \
+  --zone "us-central1-a" > /dev/null
+
+cat <<EOT > remote-install-cf.sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+export HOME=/tmp/minikube
+export PATH=/tmp/minikube/bin:/tmp/minikube/go/bin:$PATH
+
+CF_VALUES=/tmp/cf-install-values.yml
+CF_RENDERED=/tmp/cf-rendered.yml
+cd /tmp/minikube/cf-for-k8s
+ytt -f config -f ci/tasks/install-cf-on-minikube/only-nodejs-builder-overlay.yml -f \$CF_VALUES > \$CF_RENDERED
+
+eval "$(minikube docker-env)"
+kapp deploy -f \$CF_RENDERED -a cf -y
+EOT
+chmod +x remote-install-cf.sh
+
+echo "Uploading remote-install-cf.sh..."
+gcloud beta compute \
+  scp remote-install-cf.sh ${user_host}:/tmp \
+  --zone "us-central1-a" > /dev/null
+
+echo "Running remote-install-cf.sh..."
+gcloud beta compute \
+  ssh ${user_host} \
+  --command "/tmp/remote-install-cf.sh" \
+  --zone "us-central1-a"
